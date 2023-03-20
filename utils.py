da8e94cbba158355feadba4b48ed64b87d58b090
cpumarfrohberg <cpumarfrohberg@gmail.com>
2023-03-20 22:46:44 +0100

    add new utils.py version

--- utils.py
+++ utils.py
@@ -4,8 +4,6 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-#import pytest
-
 
 PATH = './data'
 
@@ -36,10 +34,6 @@ class BikeThefts():
         '''Return DataFrame with feature matrix and labels as values.'''
         df = pd.read_csv(f'{self.path}/{file}.csv', index_col=0, parse_dates=True, encoding = 'unicode_escape')
         return df
-    
-    def check_unique(self, series: pd.Series) -> pd.Series:
-        '''Return unique values of selected columns.'''
-        return series.unique()
 
     def include_timestamps(self, df: pd.DataFrame) -> pd.DataFrame:
         '''Return DataFrame with time-stamps.'''
@@ -56,28 +50,11 @@ class BikeThefts():
         for col in time_parsables:
             df[col] = pd.to_datetime(df[col])
         return df
-
-    def crime_duration_days(self, df: pd.DataFrame, start, end) -> pd.DataFrame: #use for plotting?
-        '''Calculate duration of crime in days.'''
-        df['crime_duration_days'] = end - start
-        return df #check if you want to keep it 
     
-    def crime_duration_hours(self, df: pd.DataFrame, start, end) -> pd.DataFrame: #use for plotting?
+    def crime_duration_minutes(self, df: pd.DataFrame, start_time: pd.Series, end_time: pd.Series) -> pd.DataFrame:
         '''Calculate duration of crime in hours.'''
-        df['crime_duration_hours'] = abs(start - end)
-        return df #check if you want to keep it 
-    
-    def fill_ints(self, df: pd.DataFrame) -> pd.DataFrame:
-        '''Reencode LOR into 8-digit values.'''
-        df['LOR'] = df['LOR'].apply(lambda x: str(x))
-        df['LOR'] = df['LOR'].apply(lambda x: f'0{(x)}'[-8:])
+        df['time_diff'] = end_time.sub(start_time).dt.total_seconds().div(60)
         return df
-
-    def thefts_count(self, df: pd.DataFrame, frequency:str) -> pd.DataFrame:
-        '''Calculate average thefts per time-frequency.'''
-        df_filled = pd.DataFrame(df.groupby(frequency).size(),
-                          columns=[f'{frequency}_thefts_count']).reset_index()
-        return df_filled #check if you want to keep it
     
     def insert_district(self, df, dictionary):
         '''Iterate thru rows of LOR-column and parse values into district names.
@@ -117,11 +94,4 @@ class BikeThefts():
     def save_intermediate_data(self, df: pd.DataFrame, file: str):
         '''Save extracted data locally as csv-file.'''
         return df.to_csv(f'{self.path}/{file}.csv')
-
-    def save_LOR_bike_thefts(self, df, group_by: str, col_names: str, file: str):
-        '''Save extracted LOR-bike thefts-data locally as csv-file.'''
-        bike_thefts_LOR = pd.DataFrame(df.groupby(group_by).size(),
-                        columns = [col_names]).reset_index()
-        return bike_thefts_LOR.to_csv(f'{self.path}/{file}.csv') #necessary?
     
-    #TODO; include class for plotting time series?
